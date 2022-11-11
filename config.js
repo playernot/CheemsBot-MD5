5f8af23f47ba0893e53eb7b60c948a73f970cf78
krishosting <111237854+krishosting569@users.noreply.github.com>
2022-11-11 20:26:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,26 +14,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.vcardowner = ['62895711966868'] //ur owner number
+global.ownername = "🦄Rizky Store" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.socialm = "GitHub: Rizky" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213'] //ur number
-global.ownernomer = "916909137213" //ur number
-global.ownertag = '916909137213' //ur tag number
-global.premium = ['916909137213'] //ur premium number
-global.botname = 'Cheems Bot V5' //ur bot name
-global.ownername = "🦄Dream Guy Xeon"
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
+global.owner = ['62895711966868'] //ur number
+global.ownernomer = "62895711966868" //ur number
+global.ownertag = '62895711966868' //ur tag number
+global.premium = ['62895711966868'] //ur premium number
+global.botname = 'Rizky Bot' //ur bot name
+global.ownername = "🦄Rizky Store"
+global.linkz = "https://youtu.be/krishosting" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD5' //script link
+global.botscript = 'https://github.com/kris/Rizky' //script link
 global.themeemoji = "🦄" //ur theme emoji
 global.packname = "Sticker By" //ur sticker watermark packname
-global.author = "Cheems Bot" //ur sticker watermark author
-global.wm = "Xeon Bot Inc." //ur watermark
+global.author = "Rizky Bot" //ur sticker watermark author
+global.wm = "Rizky Store." //ur watermark
 
 // Other
 global.sessionName = 'session'
